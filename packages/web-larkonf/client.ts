a186f0ae648fa97e00b1cce9b57ed25af5f17b01
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2021-09-21 13:32:46 +0200

    fix: specify api version for Sanity client

--- packages/web-larkonf/client.ts
+++ packages/web-larkonf/client.ts
@@ -3,6 +3,7 @@ import sanityClient from "@sanity/client";
 const client = sanityClient({
   projectId: "qy7enua2",
   dataset: "production",
+  apiVersion: "2020-09-20",
   token: "", // or leave blank to be anonymous user
   useCdn: false, // `false` if you want to ensure fresh data
 });
