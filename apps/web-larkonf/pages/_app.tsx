61d0b40fa39dd02688c81caea5aec4ce62a03fa2
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2022-01-04 09:28:59 +0100

    fix: script loading issue

--- apps/web-larkonf/pages/_app.tsx
+++ apps/web-larkonf/pages/_app.tsx
@@ -1,9 +1,22 @@
+import Script from "next/script";
 import { DefaultSeo } from "next-seo";
 import "design/dist/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-82KH03X2WJ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-82KH03X2WJ',{'anonymize_ip':true });
+          `}
+      </Script>
       <DefaultSeo
         additionalLinkTags={[
           {
--- apps/web-larkonf/pages/_document.tsx
+++ apps/web-larkonf/pages/_document.tsx
@@ -24,19 +24,6 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
           />
           <link rel="stylesheet" href={global.fontUrl} />
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-82KH03X2WJ"
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-82KH03X2WJ',{'anonymize_ip':true });`,
-            }}
-          />
         </Head>
         <body>
           <Main />
